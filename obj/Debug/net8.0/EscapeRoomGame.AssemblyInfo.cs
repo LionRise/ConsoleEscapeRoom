8d118795a1d647bf9028d164d3144767f67bc515
Stormhuf44 <139475496+Stormhuf44@users.noreply.github.com>
2024-06-30 15:28:07 +0200

    Afterlife

--- obj/Debug/net8.0/EscapeRoomGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/EscapeRoomGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EscapeRoomGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdcc5a1899e1dd9b86ece8d17dfe05a013adb07a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88f92d791a8162b916f104a167b718a15bedd40")]
 [assembly: System.Reflection.AssemblyProductAttribute("EscapeRoomGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EscapeRoomGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
